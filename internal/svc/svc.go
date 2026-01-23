6b2c28c7d10a79b17d4a83ab49848d92cd5ff390
zhangdongping <zhangdongping@ut.cn>
2026-01-23 17:29:47 +0800

    feat: 添加 Kubernetes 客户端到服务上下文
    引入 k8s.io/client-go 依赖，为 ServiceContext 结构体添加 Clientset 字段。
    新增 NewServiceContext 构造函数，自动创建集群内配置的 Kubernetes 客户端，
    以便服务能够与 Kubernetes API 进行交互。

--- internal/svc/svc.go
+++ internal/svc/svc.go
@@ -1,5 +1,25 @@
 package svc
 
-type ServiceContext struct {
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
 
+type ServiceContext struct {
+	Clientset *kubernetes.Clientset
+}
+
+func NewServiceContext() *ServiceContext {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return &ServiceContext{
+		Clientset: clientset,
+	}
 }
